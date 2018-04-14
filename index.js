0022b22f74b5819f632b5a0cfd3c49916e0481c7
Andrew Jarombek <ajarombek95@gmail.com>
2018-04-13 22:38:57 -0400

    Hello World Node Command Line Script

---
+++ index.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+console.info("Node Shell Script Says: Hello World!");
---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Equivalent to npm init
+yarn init
+
+# Install globally to link the script in the package.json to a location on the path
+npm install -g
+
+# Now you can call the script directly!
+tokenize
